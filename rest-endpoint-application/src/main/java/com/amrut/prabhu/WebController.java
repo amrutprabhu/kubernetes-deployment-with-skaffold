b5d0fb280eb13d1fdd6dd7c2a7608f975c2fcaaf
amrutprabhu <amrutprabhu42@gmail.com>
2022-11-01 20:59:12 +0100

    Adding initial project

---
+++ rest-endpoint-application/src/main/java/com/amrut/prabhu/WebController.java
@@ -0,0 +1,13 @@
+package com.amrut.prabhu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @GetMapping("/")
+    public String getValue() {
+        return "Yes! it works";
+    }
+}
